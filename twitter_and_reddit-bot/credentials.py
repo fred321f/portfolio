a4b359109432e60939e7f36a08a7b74ec16e7e82
fred321f <frederick01nielsen@gmail.com>
2023-07-03 18:18:00 +0200

    Create credentials.py

---
+++ twitter_and_reddit-bot/credentials.py
@@ -0,0 +1,16 @@
+import tweepy
+
+# Replace all X's with your own credentials!
+twitter_consumer_key = 'X'
+twitter_consumer_secret = 'X'
+twitter_access_token = 'X'
+twitter_access_token_secret = 'X'
+
+reddit_client_id = 'X'
+reddit_client_secret = 'X'
+reddit_user_agent = 'X'
+username = "X"
+password = "X"
+
+instagram_client_id = ''
+instagram_client_secret = ''
