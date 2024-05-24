dbf87ab93f690717363cc928dfb7eb328bfb86c6
Frederick L. Nielsen <frederick01nielsen@gmail.com>
2024-05-24 22:59:15 +0200

    Added localstorage to react todo-app

--- React/todo-app/src/App.jsx
+++ React/todo-app/src/App.jsx
@@ -1,19 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
-import './App.css';
 
 function App() {
-  // Initialize todos from local storage
-  const [todos, setTodos] = useState(() => {
-    const savedTodos = localStorage.getItem('todos');
-    return savedTodos ? JSON.parse(savedTodos) : [];
-  });
-
-  // Save todos to local storage whenever they change
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
+  const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
     const newTodos = [...todos, todo];
@@ -33,7 +23,7 @@ function App() {
   };
 
   return (
-    <div className="container">
+    <div>
       <h1>Todo List</h1>
       <TodoInput addTodo={addTodo} />
       <TodoList todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
